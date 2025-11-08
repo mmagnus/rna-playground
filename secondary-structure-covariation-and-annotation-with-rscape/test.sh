ee3e10167c496c325b18b7f4c3d4ec24a27ebe5e
mmagnus <mag_dex@o2.pl>
2025-11-08 12:03:25 -0500

    fix --pdb for  secondary-structure-covariation-and-annotation-with-rscape

--- secondary-structure-covariation-and-annotation-with-rscape/test.sh
+++ secondary-structure-covariation-and-annotation-with-rscape/test.sh
@@ -1,2 +1,2 @@
-R-scape --rna --outprep 1ehz_RF00005.sto > 1ehz_RF00005_rscape.txt
-R-scape --rna --outprep 1ehz_RF00005.sto --pdb ./1ehz_A.pdb > 1ehz_RF00005_rscape_pdb.txt
+#R-scape --rna --outprep 1ehz_RF00005.sto > 1ehz_RF00005_rscape.txt
+R-scape --rna --pdb 1ehz_A.pdb --outprep 1ehz_RF00005.sto > 1ehz_RF00005_rscape_pdb.txt
